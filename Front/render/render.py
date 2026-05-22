acdb037cbf4f89b500f9f353ab970a145b5021cf
Eliel Júlio Soares da Silva <elieljuliosoaresdasilva@gmail.com>
2026-05-22 11:02:09 -0300

    .

--- Front/render/render.py
+++ Front/render/render.py
@@ -4,6 +4,8 @@ from reportlab.lib.pagesizes import A4 #(595.2755905511812, 841.8897637795277)
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
+CM = 28.3464567
+p_LINE_HEIGHT = 20
 
 def file_route(filename: str, src_path: str='render/src/imagens'):
     if not filename:
@@ -12,29 +14,24 @@ def file_route(filename: str, src_path: str='render/src/imagens'):
     file_path = os.path.join(base_dir, '..', src_path, filename)
     return os.path.normpath(file_path)
 
-def render_proposta_p1(DadosProposta: dict):
+def register_font(doc, font_name='TrebuchetMS.ttf', call='TrebuchetMS'):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    font_path = os.path.join(base_dir, 'src', font_name)
+    if os.path.exists(font_path):pdfmetrics.registerFont(TTFont(call, font_path))
+    else: raise FileNotFoundError(f"Font file '{font_name}' not found at path: {font_path}")
+        
+def render_proposta_p1(DadosProposta: dict, documento=None):
     if not DadosProposta:
         raise ValueError("DadosProposta não pode ser vazio")
-    p_LINE_HEIGHT = 20
-
-
-    documento = canvas.Canvas(f"proposta_{DadosProposta['id']}.pdf", pagesize=A4)
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    font_path = os.path.join(base_dir, 'src', 'TrebuchetMS.ttf')
-    font_name = 'TrebuchetMS'
-    if os.path.exists(font_path):pdfmetrics.registerFont(TTFont(font_name, font_path))
-
 
     # Tentar registrar variante Bold (TrebuchetMS-Bold.ttf)
-    bold_font_path = os.path.join(base_dir, 'src', 'Trebuchet MS Bold.ttf')
-    bold_font_name = font_name + '-Bold'
-    if os.path.exists(bold_font_path):pdfmetrics.registerFont(TTFont(bold_font_name, bold_font_path))
+    # bold_font_path = os.path.join(base_dir, 'src', 'Trebuchet MS Bold.ttf')
+    # bold_font_name = font_name + '-Bold'
+    # if os.path.exists(bold_font_path):pdfmetrics.registerFont(TTFont(bold_font_name, bold_font_path))
 
     documento.drawImage(file_route("capa.png"), 0, 0, width=A4[0], height=A4[1])
 
-
-    cabeçalho_y = 300
-    tamanho_fonte = 18
+    cabeçalho_y, tamanho_fonte = 300, 18
 
     linhas_cabecalho = [
         ("Proposta Nº: ", f"{DadosProposta['id']}", ""),
@@ -44,18 +41,19 @@ def render_proposta_p1(DadosProposta: dict):
         ("Cidade: ", f"{DadosProposta.get('cidade-uf', 'N/A')}", "")
     ]
 
-    for i, (rotulo, valor, sufixo) in enumerate(linhas_cabecalho, start=1):
-        y_atual = cabeçalho_y - i * p_LINE_HEIGHT
-        x_atual = 75
+    register_font(documento, font_name='TrebuchetMS.ttf', call='TrebuchetMS')
+    register_font(documento, font_name='Trebuchet MS Bold.ttf', call='TrebuchetMS-Bold')
 
-        documento.setFont(bold_font_name, tamanho_fonte)
+    for i, (rotulo, valor, sufixo) in enumerate(linhas_cabecalho, start=1):
+        y_atual, x_atual = cabeçalho_y - i * p_LINE_HEIGHT, 75
+
+        documento.setFont('TrebuchetMS-Bold', tamanho_fonte)
         documento.drawString(x_atual, y_atual, rotulo)
 
-        x_atual += documento.stringWidth(rotulo, bold_font_name, tamanho_fonte)
+        x_atual += documento.stringWidth(rotulo, 'TrebuchetMS-Bold', tamanho_fonte)
 
-        documento.setFont(font_name, tamanho_fonte)
+        documento.setFont('TrebuchetMS', tamanho_fonte)
         documento.drawString(x_atual, y_atual, f"{valor}{sufixo}")
-    documento.setFont(font_name, 12)
 
     return documento
 
@@ -77,14 +75,36 @@ def render_proposta_p4(dados: dict, documento):
     return documento
 
 def render_proposta_p5(dados: dict, documento):
+    register_font(documento, font_name='TrebuchetMS.ttf', call='TrebuchetMS')
+    register_font(documento, font_name='Trebuchet MS Bold.ttf', call='TrebuchetMS-Bold')
     documento.showPage()
-    documento.drawImage(file_route("dados do cliente.png"), 15, A4[1]-100, width=100, height=30)
+    documento.drawImage(file_route("dados do cliente.png"), 15, A4[1]-(1.59*CM)-15, width=8.94*CM, height=1.59*CM)
+    dados_cliente = [
+        ("Cliente: ", dados.get("nome_completo", "N/A")),
+        ("Telefone: ", dados.get("telefone", "N/A")),
+        ("Endereço: ", dados.get("endereco", "N/A")),
+        ("Email: ", dados.get("email", "N/A")),
+    ]
+    y = A4[1] - (1.59 * CM) - 15 - 10- p_LINE_HEIGHT
+    for rotulo, valor in dados_cliente:
+        documento.setFont('TrebuchetMS-Bold', 12)
+        documento.drawString(CM, y, rotulo)
+        documento.setFont('TrebuchetMS', 12)
+        documento.drawString(documento.stringWidth(rotulo, 'TrebuchetMS-Bold', 12)+CM, y, valor)
+        y -= p_LINE_HEIGHT
+
+    y -= (p_LINE_HEIGHT-15-16.15*CM)
+    documento.drawImage(file_route("capacidade.png"), 15, y, width=16.15*CM, height=1.63*CM)
+    print("Dados do cliente renderizados com sucesso.")
+    documento.drawString(CM, y, "teste")
+
     return documento
 def render_proposta(dados: dict):
-    documento = render_proposta_p1(dados)
-    documento = render_proposta_p2(dados, documento)
-    documento = render_proposta_p3(dados, documento)
-    documento = render_proposta_p4(dados, documento)
+    documento = canvas.Canvas(f"proposta_{dados['id']}.pdf", pagesize=A4)
+    # documento = render_proposta_p1(dados, documento)
+    # documento = render_proposta_p2(dados, documento)
+    # documento = render_proposta_p3(dados, documento)
+    # documento = render_proposta_p4(dados, documento)
     documento = render_proposta_p5(dados, documento)
     return documento
 
